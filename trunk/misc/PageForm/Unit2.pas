378c524c0fea10e1a04df1da59cb8aa720c2d3c2
silvioprog <silvioprog@44b2178c-fcf6-676f-3f0a-1c1e759a4f45>
2010-07-14 22:11:39 +0000

    Adiciona projeto que mostra como usar Form em abas.

---
+++ trunk/misc/PageForm/Unit2.pas
@@ -0,0 +1,24 @@
+unit Unit2; 
+
+{$mode objfpc}
+
+interface
+
+uses
+  LResources, Forms, StdCtrls;
+
+type
+
+  { TForm2 }
+
+  TForm2 = class(TForm)
+    Label1: TLabel;
+  end;
+
+implementation
+
+initialization
+  {$I Unit2.lrs}
+
+end.
+
