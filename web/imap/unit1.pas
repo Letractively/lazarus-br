0b851e106a7fcf90741afed1fc1836fdc180ebe0
silvioprog <silvioprog@44b2178c-fcf6-676f-3f0a-1c1e759a4f45>
2012-09-06 02:16:52 +0000

    + Adiciona ótimo exemplo de como acessar uma conta IMAP usando Synapse.

---
+++ web/imap/unit1.pas
@@ -0,0 +1,71 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, Forms, StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    ListBox1: TListBox;
+    Memo1: TMemo;
+    procedure Button1Click(Sender: TObject);
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+uses
+  imapsend;
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  imap: TImapSend;
+  n: integer;
+  sl: TStringList;
+begin
+  sl := TStringList.Create;
+  imap := TImapSend.Create;
+  imap.UserName := 'test@silvioprog.com.br';
+  imap.Password := 'Abc$123654';
+  imap.TargetHost := 'imap.silvioprog.com.br';
+  if imap.Login then
+  begin
+    Memo1.Clear;
+    // get the folder list into ListBox1
+    if ListBox1.Items.Count = 0 then
+    begin
+      imap.List('', ListBox1.Items);
+      ListBox1.ItemIndex := 0;
+    end;
+    // select the Inbox
+    if imap.SelectFolder(ListBox1.GetSelectedText) then
+    begin
+      // loop through the items
+      for n := 1 to imap.SelectedCount do
+      begin
+        // get each message's header
+        if imap.FetchHeader(n, sl) then
+        begin
+          // and append them to a TMemo
+          Memo1.Lines.AddStrings(sl);
+          Memo1.Lines.Append('');
+        end;
+      end;
+      imap.CloseFolder;
+    end;
+    imap.Logout;
+  end;
+  imap.Free;
+  sl.Free;
+end;
+
+end.
+
