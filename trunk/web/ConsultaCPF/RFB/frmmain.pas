73df289767ab8fcd5b53acf880a78dd06185eced
silvioprog <silvioprog@44b2178c-fcf6-676f-3f0a-1c1e759a4f45>
2012-11-16 14:45:39 +0000

    + Adiciona unit RFB.pas.

--- trunk/web/ConsultaCPF/RFB/frmmain.pas
+++ trunk/web/ConsultaCPF/RFB/frmmain.pas
@@ -5,8 +5,7 @@ unit frmMain;
 interface
 
 uses
-  HttpUtils, RUtils, HttpSend, SynaUtil, Classes, SysUtils, Forms, Dialogs,
-  StdCtrls, ExtCtrls, FPJSON, LCLIntf;
+  RFB, Classes, SysUtils, Forms, Dialogs, StdCtrls, ExtCtrls, FPJSON, LCLIntf;
 
 type
   TfrMain = class(TForm)
@@ -26,27 +25,7 @@ type
     FGuid: string;
     FState: string;
     FCookie: string;
-  public
-    procedure Prepare;
-    procedure GetCaptcha;
-    procedure Query;
   end;
-
-const
-  API_DOMAIN_URL = 'http://api.silvioprog.com.br/rfb.brook/cpf/';
-  API_PREPARE_URL = API_DOMAIN_URL + 'prepare';
-  API_CAPTCHA_URL = API_DOMAIN_URL + 'captcha/%s';
-  API_QUERY_URL = API_DOMAIN_URL + 'query/%s';
-  API_QUERY_FORM = 'cookie=%s&state=%s&captcha=%s';
-
-var
-  SAPIPrepareError: string = 'Não foi possível obter dados iniciais.';
-  SAPICaptchaError: string = 'Não foi possível obter Captcha.';
-  SAPIQueryError: string = 'Não foi possível obter dados para o documento "%s".';
-  SEmptyDocumentError: string = 'Por favor, informe o número do documento.';
-  SEmptyCaptchaError: string =
-    'Por favor, informe corretamente os caracteres exibidos na imagem ao lado.' +
-    LineEnding + 'Caso não esteja vendo os caractes, clique em "Obter nova imagem".';
 
 var
   frMain: TfrMain;
@@ -56,9 +35,17 @@ implementation
 {$R *.lfm}
 
 procedure TfrMain.btGetCaptchaClick(Sender: TObject);
+var
+  VImage: TStream = nil;
 begin
-  Prepare;
-  GetCaptcha;
+  RFBPrepare(FGuid, FState, FCookie);
+  try
+    RFBGetCaptcha(VImage, FGuid);
+    imCaptcha.Picture.Clear;
+    imCaptcha.Picture.LoadFromStream(VImage);
+  finally
+    FreeAndNil(VImage);
+  end;
 end;
 
 procedure TfrMain.edDocumentKeyPress(Sender: TObject; var Key: char);
@@ -69,76 +56,7 @@ begin
 end;
 
 procedure TfrMain.btQueryClick(Sender: TObject);
-begin
-  Query;
-end;
-
-procedure TfrMain.edCaptchaKeyPress(Sender: TObject; var Key: char);
-begin
-  if Key = #13 then
-  begin
-    Query;
-    Key := #0;
-  end;
-end;
-
-procedure TfrMain.FormShow(Sender: TObject);
-begin
-  Prepare;
-  GetCaptcha;
-end;
-
-procedure TfrMain.Prepare;
 var
-  VHttp: THttpSend;
-  VJSON: TJSONObject = nil;
-begin
-  VHttp := THttpSend.Create;
-  try
-    if HttpRequest(VHttp, 'GET', API_PREPARE_URL) then
-    begin
-      GetJSONObject(VHttp.Document, VJSON);
-      FGuid := VJSON['guid'].AsString;
-      FState := VJSON['state'].AsString;
-      FCookie := VJSON['cookie'].AsString;
-    end
-    else
-      ShowMessage(SAPIPrepareError);
-  finally
-    FreeAndNil(VJSON);
-    VHttp.Free;
-  end;
-end;
-
-procedure TfrMain.GetCaptcha;
-var
-  VHttp: THttpSend;
-  VImage: TMemoryStream;
-  VJSON: TJSONObject = nil;
-begin
-  VHttp := THttpSend.Create;
-  VImage := TMemoryStream.Create;
-  try
-    imCaptcha.Picture.Clear;
-    if HttpRequest(VHttp, 'GET', Format(API_CAPTCHA_URL, [FGuid])) then
-    begin
-      GetJSONObject(VHttp.Document, VJSON);
-      Base64ToStream(VJSON['image'].AsString, VImage);
-      VImage.Position := 0;
-      imCaptcha.Picture.LoadFromStream(VImage);
-    end
-    else
-      ShowMessage(SAPICaptchaError);
-  finally
-    VImage.Free;
-    FreeAndNil(VJSON);
-    VHttp.Free;
-  end;
-end;
-
-procedure TfrMain.Query;
-var
-  VHttp: THttpSend;
   VJSON: TJSONObject = nil;
 begin
   if Trim(edDocument.Text) = '' then
@@ -153,27 +71,32 @@ begin
     edCaptcha.SetFocus;
     Exit;
   end;
-  VHttp := THttpSend.Create;
   try
-    WriteStrToStream(VHttp.Document,
-      Format(API_QUERY_FORM, [FCookie, FState, edCaptcha.Text]));
-    VHttp.MimeType := 'application/x-www-form-urlencoded';
-    if HttpRequest(VHttp, 'POST', Format(API_QUERY_URL, [edDocument.Text])) then
-    begin
-      GetJSONObject(VHttp.Document, VJSON);
-      ShowMessageFmt('Nome: %s' + LineEnding + 'Situação: %s' + LineEnding +
-        'Documento: %s', [VJSON['name'].AsString, VJSON['status'].AsString,
-        VJSON['querieddocument'].AsString]);
-    end
-    else
-      ShowMessage(Format(SAPIQueryError, [edDocument.Text]));
-  finally
+    RFBQuery(VJSON, FCookie, FState, edCaptcha.Text, edDocument.Text);
+    ShowMessageFmt('Nome: %s' + LineEnding + 'Situação: %s' + LineEnding +
+      'Documento: %s', [VJSON['name'].AsString, VJSON['status'].AsString,
+      VJSON['querieddocument'].AsString]);
     edDocument.Clear;
+    btGetCaptcha.SetFocus;
     edCaptcha.Clear;
     imCaptcha.Picture.Clear;
+  finally
     FreeAndNil(VJSON);
-    VHttp.Free;
   end;
+end;
+
+procedure TfrMain.edCaptchaKeyPress(Sender: TObject; var Key: char);
+begin
+  if Key = #13 then
+  begin
+    btQueryClick(Sender);
+    Key := #0;
+  end;
+end;
+
+procedure TfrMain.FormShow(Sender: TObject);
+begin
+  btGetCaptchaClick(Sender);
 end;
 
 end.
