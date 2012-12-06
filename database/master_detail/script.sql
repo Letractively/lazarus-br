b2e24ca45f67244306f7b73f5c3ca544307a29d2
silvioprog <silvioprog@44b2178c-fcf6-676f-3f0a-1c1e759a4f45>
2012-12-06 13:30:37 +0000

    + Adiciona exemplo simples de master-detail com SQLdb.

---
+++ database/master_detail/script.sql
@@ -0,0 +1,19 @@
+﻿create table person (
+  id serial not null,
+  name character varying(50) not null,
+  constraint pk_person
+  primary key (id)
+);
+
+create table phone (
+  id serial not null,
+  personid integer not null,
+  number character varying(10),
+  constraint pk_phone
+    primary key (id ),
+  constraint fk_phone
+    foreign key (personid)
+    references person (id)
+    on update cascade
+    on delete cascade
+);
