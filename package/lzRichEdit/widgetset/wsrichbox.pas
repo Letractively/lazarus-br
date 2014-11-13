8def6d65fae2c6f24ee4c6632d37b003ebbb90d2
elsonjuniog <elsonjuniog@gmail.com>
2014-11-13 04:32:23 +0000

    --Alterar licença

--- package/lzRichEdit/widgetset/wsrichbox.pas
+++ package/lzRichEdit/widgetset/wsrichbox.pas
@@ -3,29 +3,31 @@ lzRichEdit
 
 Copyright (C) 2010 Elson Junio elsonjunio@yahoo.com.br
 
-This library is free software; you can redistribute it and/or modify it
-under the terms of the GNU Library General Public License as published by
-the Free Software Foundation; either version 2 of the License, or (at your
-option) any later version with the following modification:
+This is the file COPYING.modifiedLGPL, it applies to several units in the
+Lazarus sources distributed by members of the Lazarus Development Team.
+All files contains headers showing the appropriate license. See there if this
+modification can be applied.
+
+These files are distributed under the Library GNU General Public License
+(see the file COPYING.LGPL) with the following modification:
 
 As a special exception, the copyright holders of this library give you
 permission to link this library with independent modules to produce an
-executable, regardless of the license terms of these independent modules,and
-to copy and distribute the resulting executable under terms of your choice,
+executable, regardless of the license terms of these independent modules,
+and to copy and distribute the resulting executable under terms of your choice,
 provided that you also meet, for each linked independent module, the terms
 and conditions of the license of that module. An independent module is a
-module which is not derived from or based on this library. If you modify
-this library, you may extend this exception to your version of the library,
-but you are not obligated to do so. If you do not wish to do so, delete this
+module which is not derived from or based on this library. If you modify this
+library, you may extend this exception to your version of the library, but
+you are not obligated to do so. If you do not wish to do so, delete this
 exception statement from your version.
 
-This library is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library General Public License
-for more details.
 
-You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+If you didn't receive a copy of the file COPYING.LGPL, contact:
+      Free Software Foundation, Inc.,
+      675 Mass Ave
+      Cambridge, MA  02139
+      USA
 }
 
 unit WSRichBox;
@@ -294,4 +296,4 @@ end;
 
 
 end.
-
+
